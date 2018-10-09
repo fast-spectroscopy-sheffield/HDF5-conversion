73cb0239d49a0bef77588085f12b99923756fba7
Fast Spectroscopy Group at the University of Sheffield <43988660+fast-spectroscopy-sheffield@users.noreply.github.com>
2018-10-09 11:51:09 +0100

    added python files
    Transferred files to GitHub and added a brief README to describe current app usage.

---
+++ code/HDF5.bat
@@ -0,0 +1,6 @@
+call C:\ProgramData\Miniconda3\Scripts\activate.bat C:\ProgramData\Miniconda3
+call cd C:\Users\Lab_Test
+call conda activate pyTAtest
+call cd Documents\GitHub\HDF5-conversion\code
+call python app.py
+call cd C:\Users\Lab_Test
